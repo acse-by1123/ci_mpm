175517f252110dc2dd6252cf7b08621a921f78ff
acse-1bd5843a <b.yildizgoerer+1@gmail.com>
2023-10-17 11:02:46 +0100

    combined functions1.py and constants.py to avoid errors

--- simple_functions/functions1.py
+++ simple_functions/functions1.py
@@ -1,6 +1,8 @@
+from numpy import sqrt
+from simple_functions.functions1 import factorial
 from functools import cache
 
-__all__ = ['my_sum', "my_factorial"]
+__all__ = ['my_sum', "my_factorial", "pi"]
 
 
 def my_sum(iterable):
@@ -13,3 +15,16 @@ def my_sum(iterable):
 @cache
 def my_factorial(n):
     return n * my_factorial(n-1) if n else 1
+
+
+__all__ = ['pi']
+
+
+def pi(terms=1):
+    return 1./(2.*sqrt(2.)/9801.*rsum(terms))
+
+
+@cache
+def rsum(n):
+    t = factorial(4*n)*(1103+26390*n)/(factorial(n)**4*396**(4*n))
+    return t + rsum(n-1) if n else t
